07f6476a0c605752a9c82e26fcc72b6269f8c0ac
Renato-python <pellegrin80@hotmail.com>
2026-01-31 21:50:43 -0500

    cheking the workflow

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,10 +9,10 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "tf-resources-gha"
+    bucket         = "tf-resources-git-actions-2026"
     region         = "us-east-1"
     key            = "github-actions/terraform.tfstate"
     encrypt        = true
-    dynamodb_table = "tf-resources-gha-lock"
+    dynamodb_table = "tf-resouces-gha-lock"
   }
 }
